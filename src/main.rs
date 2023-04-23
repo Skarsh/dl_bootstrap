17d33afa2139f3acbb8f9479bb51f4016246ceb6
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2023-04-23 23:33:44 +0200

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+use std::process::Command;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    driver: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.driver {
+        let mut driver_version = Command::new("nvidia-smi");
+        driver_version.status().expect("process failed to execute");
+    }
+
+
+    println!();
 }
